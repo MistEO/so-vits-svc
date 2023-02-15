17ef7c57853b054eb121c62765b02ba92ae468b1
MistEO <mistereo@hotmail.com>
2023-02-15 14:30:42 +0800

    perf: 支持多格式音频输入

--- inference_caller.py
+++ inference_caller.py
@@ -29,17 +29,15 @@ hubert_path = sys.argv[3]
 
 svc_model = Svc(model_path, config_path, hubert_path)
 
-raw_audio_path = sys.argv[4]  # 仅支持 wav
+input_path = Path(sys.argv[4])
 key = int(sys.argv[5])  # 音高调整，支持正负（半音）
 spk = sys.argv[6]
 slice_db = int(sys.argv[7])  # 默认-40，嘈杂的音频可以-30，干声保留呼吸可以-50
 output = Path(sys.argv[8])
-wav_format = sys.argv[9]  # 音频输出格式
+output_format = sys.argv[9]  # 音频输出格式
 
-infer_tool.format_wav(raw_audio_path)
-wav_path = Path(raw_audio_path).with_suffix('.wav')
-chunks = slicer.cut(wav_path, db_thresh=slice_db)
-audio_data, audio_sr = slicer.chunks2audio(wav_path, chunks)
+chunks = slicer.cut(input_path, db_thresh=slice_db)
+audio_data, audio_sr = slicer.chunks2audio(input_path, chunks)
 
 audio = []
 for (slice_tag, data) in audio_data:
@@ -58,6 +56,6 @@ for (slice_tag, data) in audio_data:
         _audio = out_audio.cpu().numpy()
     audio.extend(list(_audio))
 
-res_path = output / f'{wav_path.parent.stem}_{key}key_{spk}.{wav_format}'
+res_path = output / f'{input_path.parent.stem}_{key}key_{spk}.{output_format}'
 os.makedirs(output, exist_ok=True)
-soundfile.write(res_path, audio, svc_model.target_sample, format=wav_format)
+soundfile.write(res_path, audio, svc_model.target_sample, format=output_format)
